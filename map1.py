8d0c49d2747eceaf0e65171bd9fb2c3745a97261
Obinna Uzozie <Uzozie.obi@icloud.com>
2022-04-22 13:15:21 +0800

    using volcanoes.txt to render the volcanoes on the map

--- map1.py
+++ map1.py
@@ -1,4 +1,17 @@
 import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+
+#created two list from the volcanoes.txt using python
+lat = list(data["LAT"])
+lon = list(data["LON"])
+
+
+
+
+
+
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles= "Stamen Terrain")
 #we add elements or obejcts into the map from here 
 #1.. Adding Map Markers
@@ -6,14 +19,16 @@ fg = folium.FeatureGroup(name="MyMap")
 #fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a Marker", icon=folium.Icon(color="purple")))
 
 #****** USING FOR LOOPS to ADD MULTIPLE MARKERS
-for coordinates in [[38.2, -99.1], [39.2, -97.1]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="Hi I am a Marker", icon=folium.Icon(color="purple")))
+#this is a data frame
+#for coordinates in [[38.2, -99.1], [39.2, -97.1]]:
+
+
+
+#to iterate over a new list
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln], popup="Hi I am a Marker", icon=folium.Icon(color="purple")))
 
 
 map.add_child(fg)
-
-
-
-
 
 map.save("Map1.html")
