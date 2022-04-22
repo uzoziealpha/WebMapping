d337394ed97e5db39117de2fe74f61a41acd4a2b
Obinna Uzozie <Uzozie.obi@icloud.com>
2022-04-22 11:55:26 +0800

    using for loops to multiply markers

--- map1.py
+++ map1.py
@@ -3,8 +3,16 @@ map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles= "Stamen Terrain"
 #we add elements or obejcts into the map from here 
 #1.. Adding Map Markers
 fg = folium.FeatureGroup(name="MyMap")
-fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a Marker", icon=folium.Icon(color="purple")))
+#fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a Marker", icon=folium.Icon(color="purple")))
+
+#****** USING FOR LOOPS to ADD MULTIPLE MARKERS
+for coordinates in [[38.2, -99.1], [39.2, -97.1]]:
+    fg.add_child(folium.Marker(location=coordinates, popup="Hi I am a Marker", icon=folium.Icon(color="purple")))
+
+
 map.add_child(fg)
+
+
 
 
 
